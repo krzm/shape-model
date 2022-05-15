8eedffbdcdd61c3d2f094556a814ec65b01e8c4d
krzm <kmazanek@gmail.com>
2022-05-15 22:12:09 +0200

    Add line data

---
+++ Shape.Model.Tests/Line.Data/LineXml.cs
@@ -0,0 +1,38 @@
+﻿using Xml.Generator;
+
+namespace Shape.Model.Tests;
+
+public class LineXml
+    : IText
+{
+    public string Text =>
+        $"<?xml version=\"1.0\" encoding=\"utf-8\"?>{Environment.NewLine}" +
+        $"<Line>{Environment.NewLine}" +
+        $"  <Context>Physic</Context>{Environment.NewLine}" +
+        $"  <Color>{Environment.NewLine}" +
+        $"    <A>255</A>{Environment.NewLine}" +
+        $"    <R>255</R>{Environment.NewLine}" +
+        $"    <G>0</G>{Environment.NewLine}" +
+        $"    <B>0</B>{Environment.NewLine}" +
+        $"    <ScA>1</ScA>{Environment.NewLine}" +
+        $"    <ScR>1</ScR>{Environment.NewLine}" +
+        $"    <ScG>0</ScG>{Environment.NewLine}" +
+        $"    <ScB>0</ScB>{Environment.NewLine}" +
+        $"  </Color>{Environment.NewLine}" +
+        $"  <MassCenter>{Environment.NewLine}" +
+        $"    <X>334</X>{Environment.NewLine}" +
+        $"    <Y>680</Y>{Environment.NewLine}" +
+        $"  </MassCenter>{Environment.NewLine}" +
+        $"  <Velocity>{Environment.NewLine}" +
+        $"    <X>0</X>{Environment.NewLine}" +
+        $"    <Y>0</Y>{Environment.NewLine}" +
+        $"  </Velocity>{Environment.NewLine}" +
+        $"  <IsColorFilled>True</IsColorFilled>{Environment.NewLine}" +
+        $"  <Mass>10</Mass>{Environment.NewLine}" +
+        $"  <RelativeImagePath />{Environment.NewLine}" +
+        $"  <SecondPoint>{Environment.NewLine}" +
+        $"    <X>299</X>{Environment.NewLine}" +
+        $"    <Y>713</Y>{Environment.NewLine}" +
+        $"  </SecondPoint>{Environment.NewLine}" +
+        $"</Line>";
+}
---
+++ Shape.Model.Tests/Line.Data/LineXmlNumbered.cs
@@ -0,0 +1,38 @@
+﻿using Xml.Generator;
+
+namespace Shape.Model.Tests;
+
+public class LineXmlNumbered
+        : IText
+{
+    public string Text =>
+        $"01	|<?xml version=\"1.0\" encoding=\"utf-8\"?>{Environment.NewLine}" +
+        $"02	|<Line>{Environment.NewLine}" +
+        $"03	|  <Context>Physic</Context>{Environment.NewLine}" +
+        $"04	|  <Color>{Environment.NewLine}" +
+        $"05	|    <A>255</A>{Environment.NewLine}" +
+        $"06	|    <R>255</R>{Environment.NewLine}" +
+        $"07	|    <G>0</G>{Environment.NewLine}" +
+        $"08	|    <B>0</B>{Environment.NewLine}" +
+        $"09	|    <ScA>1</ScA>{Environment.NewLine}" +
+        $"10	|    <ScR>1</ScR>{Environment.NewLine}" +
+        $"11	|    <ScG>0</ScG>{Environment.NewLine}" +
+        $"12	|    <ScB>0</ScB>{Environment.NewLine}" +
+        $"13	|  </Color>{Environment.NewLine}" +
+        $"14	|  <MassCenter>{Environment.NewLine}" +
+        $"15	|    <X>334</X>{Environment.NewLine}" +
+        $"16	|    <Y>680</Y>{Environment.NewLine}" +
+        $"17	|  </MassCenter>{Environment.NewLine}" +
+        $"18	|  <Velocity>{Environment.NewLine}" +
+        $"19	|    <X>0</X>{Environment.NewLine}" +
+        $"20	|    <Y>0</Y>{Environment.NewLine}" +
+        $"21	|  </Velocity>{Environment.NewLine}" +
+        $"22	|  <IsColorFilled>True</IsColorFilled>{Environment.NewLine}" +
+        $"23	|  <Mass>10</Mass>{Environment.NewLine}" +
+        $"24	|  <RelativeImagePath />{Environment.NewLine}" +
+        $"25	|  <SecondPoint>{Environment.NewLine}" +
+        $"26	|    <X>299</X>{Environment.NewLine}" +
+        $"27	|    <Y>713</Y>{Environment.NewLine}" +
+        $"28	|  </SecondPoint>{Environment.NewLine}" +
+        $"29	|</Line>";
+}
